cdc482ca08e78dbc071fbc3082b255d24ef6536f
Papa Balla SOW <sowballapapa@gmail.com>
2025-10-24 16:50:51 +0000

    feat(Config): Activation dans ESLINT des path avec @ = ./src pour les fichiers ts, tsx, js, jsx, json

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import path from "path";
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -19,6 +20,17 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+      settings: {
+        'import/resolver': {
+            typescript: {
+                project: './tsconfig.json',
+            },
+            alias: {
+                map: [['@', path.resolve(__dirname, './src')]],
+                extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+            },
+        },
+      },
     rules: {
         '@typescript-eslint/no-explicit-any': 'off',
     }
