49cd94f5ccd113187a4cec83144426b5fa76d06a
Papa Balla SOW <sowballapapa@gmail.com>
2025-11-03 14:14:03 +0000

    feat(User_Auth): Ajout de la méthode Logout

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -11,5 +11,19 @@ export const authService = {
     register: async (data: any) => {
         const response = await api.post('/users/register/',data)
         return response
+    },
+    logout: async() =>{
+        const res = await api.post('users/logout', {})
+        return res
+    },
+    tokenRefresh: async () =>{
+        const token_refresh =localStorage.getItem('token_refresh')
+        const res = await api.post('users/token/refresh/', {refresh: token_refresh})
+        if (res){
+            localStorage.setItem('token_access', res.data.access)
+            localStorage.setItem('token_refresh', res.data.refresh)
+            localStorage.setItem('user', res.data.user)
+        }
+        return res
     }
 }
