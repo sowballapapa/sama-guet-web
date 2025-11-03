904ccee11ea416290d6677c68e6da194be0f167f
Papa Balla SOW <sowballapapa@gmail.com>
2025-11-03 14:13:21 +0000

    feat(Config Axios): Ajout de la variable d'env VITE_API_URL et ajout des interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,63 @@
-import axios from 'axios';
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
+
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: API_BASE_URL,
     headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
     },
 });
 
-export default api;
+// Request interceptor : ajoute token automatiquement
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token_access");
+        if (token && config.headers) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// Response interceptor : gestion globale des erreurs
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (!error.response) {
+            toast.error("Erreur réseau, veuillez vérifier votre connexion.");
+        } else {
+            const status = error.response.status;
+
+            switch (status) {
+                case 400:
+                    toast.error("Requête invalide.");
+                    break;
+                case 401:
+                    toast.error("Non autorisé. Veuillez vous reconnecter.");
+                    // ici tu peux faire un logout automatique si besoin
+                    break;
+                case 403:
+                    toast.error("Accès refusé.");
+                    break;
+                case 404:
+                    toast.error("Ressource non trouvée.");
+                    break;
+                case 500:
+                    toast.error("Erreur serveur. Veuillez réessayer plus tard.");
+                    break;
+                default:
+                    toast.error(error.response.data?.message || "Une erreur est survenue.");
+            }
+        }
+        return Promise.reject(error);
+    }
+);
+
+
+export default api
